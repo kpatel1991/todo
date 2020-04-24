88748597ac9277fb3d00780faa2a39df746eee70
Kunjan Patel <kunjan.dk@gmail.com>
2020-04-24 12:17:38 -0400

    Added ResponseEntity for better readability.

--- src/main/java/com/kunjan/todo/todo/controllers/ToDoController.java
+++ src/main/java/com/kunjan/todo/todo/controllers/ToDoController.java
@@ -4,8 +4,11 @@ import com.kunjan.todo.todo.domain.ToDo;
 import com.kunjan.todo.todo.services.ToDoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,32 +21,35 @@ public class ToDoController {
     private ToDoService toDoService;
 
     @GetMapping(value = "/hello")
-    public String sayHello() {
-        return "Hello World!";
+    public ResponseEntity<String> sayHello() {
+        ArrayList<String> strs = new ArrayList<>();
+
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
     }
 
     @GetMapping(value = "/todos")
-    public List<ToDo> getAllToDos() {
-        return toDoService.getAllToDos();
+    public ResponseEntity<List<ToDo>> getAllToDos() {
+        List<ToDo> toDos = toDoService.getAllToDos();
+        return new ResponseEntity<>(toDos, HttpStatus.OK);
     }
 
     @GetMapping("/todos/{id}")
-    public ToDo getToDoById(@PathVariable Long id) {
-        return toDoService.getToDoById(id);
+    public ResponseEntity<ToDo> getToDoById(@PathVariable Long id) {
+        return new ResponseEntity<>(toDoService.getToDoById(id), HttpStatus.OK);
     }
 
     @PostMapping("/todos")
-    public ToDo createToDo(@RequestBody ToDo toDo) {
-        return toDoService.updateToDo(toDo);
+    public ResponseEntity<ToDo> createToDo(@RequestBody ToDo toDo) {
+        return new ResponseEntity<>(toDoService.updateToDo(toDo), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/todos/{id}")
-    public boolean deleteToDo(@PathVariable Long id) {
-        return toDoService.deleteToDo(id);
+    public ResponseEntity<Boolean> deleteToDo(@PathVariable Long id) {
+        return new ResponseEntity<>(toDoService.deleteToDo(id), HttpStatus.OK);
     }
 
     @PutMapping("/todos/{id}")
-    public ToDo updateToDo(@RequestBody ToDo updateTodo) {
-        return toDoService.updateToDo(updateTodo);
+    public ResponseEntity<ToDo> updateToDo(@RequestBody ToDo updateTodo) {
+        return new ResponseEntity<>(toDoService.updateToDo(updateTodo), HttpStatus.CREATED);
     }
 }
